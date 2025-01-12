76d687404fb584d7698c16c987c44db6884b5a39
Cameron Wong <cw@reosource.io>
2025-01-12 12:09:02 -0800

    Install system dependencies

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
+
 from invoke import run
 
 if __name__ == "__main__":
     run("rm -f ~/.config/nvim")
     run("ln -s ~/.config/cw/nvim ~/.config/nvim")
+    run("sudo apt install ripgrep unzip python-venv")

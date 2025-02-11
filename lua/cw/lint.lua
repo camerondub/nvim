a9f3b991e0e73a83223cd989361836aa4542a249
Cameron Wong <cw@reosource.io>
2025-02-11 08:12:24 -0800

    Enable virtualenv-specific pylint

--- lua/cw/lint.lua
+++ lua/cw/lint.lua
@@ -1,23 +1,38 @@
 return {
     {
-        'mfussenegger/nvim-lint', -- linting
-        event = { 'BufReadPre', 'BufNewFile' },
+        "mfussenegger/nvim-lint", -- linting
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
-            local lint = require 'lint'
+            local lint = require("lint")
             lint.linters_by_ft = {
-                markdown = { 'markdownlint' },
+                markdown = { "markdownlint" },
+                python = { "pylint" },
             }
 
-            local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-            vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
                 group = lint_augroup,
                 callback = function()
                     lint.try_lint()
                 end,
             })
+            lint.linters.pylint.cmd = "python"
+            lint.linters.pylint.args = {
+                "-m",
+                "pylint",
+                "-f",
+                "json",
+                "--from-stdin",
+                function()
+                    return vim.api.nvim_buf_get_name(0)
+                end,
+            }
         end,
-    }, {
-        'rshkarin/mason-nvim-lint',
-        ensure_installed = { 'pylint' },
-    }
+    },
+    {
+        "rshkarin/mason-nvim-lint",
+        opts = {
+            ensure_installed = { "markdownlint" },
+        },
+    },
 }

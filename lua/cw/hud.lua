fe0cbfc780c8dc75491c3a26382a5469edf473d6
Cameron Wong <cw@reosource.io>
2025-02-11 10:20:37 -0800

    Add configuration for vim-dadbod-ui

--- lua/cw/hud.lua
+++ lua/cw/hud.lua
@@ -111,9 +111,23 @@ return {
             { "<leader>dg", ":diffget ", desc = "Diff Get {BufSpec}" },
         },
     },
-    "tpope/vim-dadbod",
-    "kristijanhusak/vim-dadbod-completion",
-    "kristijanhusak/vim-dadbod-ui",
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod", lazy = true },
+            { "kristijanhusak/vim-dadbod-completion", lazy = true },
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.db_ui_execute_on_save = 0
+        end,
+    },
     {
         "stevearc/aerial.nvim",
         opts = {},

6dc872aa9e7a46f2696c2faed2cdc4184a772361
Cameron Wong <cw@reosource.io>
2025-02-11 07:08:09 -0800

    Improve lualine context
    - Remove git branch info to provide more space for filename
    - Use full relative path in filename field

--- lua/cw/hud.lua
+++ lua/cw/hud.lua
@@ -21,7 +21,20 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { options = { theme = "powerline_dark" } },
+        opts = {
+            options = {
+                theme = "powerline_dark",
+            },
+            sections = {
+                lualine_b = { "diff", "diagnostics" },
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+            },
+        },
     },
     {
         "lukas-reineke/indent-blankline.nvim",

631de84089a008b4bb94d2607c4407281c1d4c0d
Cameron Wong <cw@reosource.io>
2025-02-11 10:19:47 -0800

    Enable 'buffer' as source for completions

--- lua/cw/complete.lua
+++ lua/cw/complete.lua
@@ -44,28 +44,17 @@ return {
                         end
                     end, { "i", "s" }),
                 }),
-                sources = {
+                sources = cmp.config.sources({
                     {
                         name = "lazydev",
                         group_index = 0,
                     },
+                    { name = "buffer" },
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
                     { name = "path" },
                     { name = "vim-dadbod-completion" },
-                    {
-                        name = "buffer",
-                        option = {
-                            get_bufnrs = function()
-                                local bufs = {}
-                                for _, win in ipairs(vim.api.nvim_list_wins()) do
-                                    bufs[vim.api.nvim_win_get_buf(win)] = true
-                                end
-                                return vim.tbl_keys(bufs)
-                            end,
-                        },
-                    },
-                },
+                }),
             })
             local types = require("cmp.types")
             vim.keymap.set("n", "<leader>pe", function()

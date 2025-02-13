5508e276c4daa7ae9145cd83ced1aa1028231412
Cameron Wong <cw@reosource.io>
2025-02-13 06:14:21 -0800

    Fix quickfix window command

--- lua/config/window.lua
+++ lua/config/window.lua
@@ -27,5 +27,5 @@ vim.keymap.set("n", "<leader>cq", ":lcl<CR>", { desc = "Curr location" })
 -- quickfix window
 vim.keymap.set("n", "<leader>qn", ":cne<CR>", { desc = "Next Quickfix" })
 vim.keymap.set("n", "<leader>qp", ":cpr<CR>", { desc = "Prev Quickfix" })
-vim.keymap.set("n", "<leader>qc", ":cl<CR>", { desc = "Curr Quickfix" })
+vim.keymap.set("n", "<leader>qc", ":cc<CR>", { desc = "Curr Quickfix" })
 vim.keymap.set("n", "<leader>qq", ":ccl<CR>", { desc = "Curr Quickfix" })

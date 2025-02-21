8aa9d2f5805b89847570ca35cd9017fa8c1806dd
Cameron Wong <cw@reosource.io>
2025-02-21 04:12:02 -0800

    Disable 'buffer' completion source

--- lua/lazyplugin/complete.lua
+++ lua/lazyplugin/complete.lua
@@ -53,7 +53,7 @@ return {
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
                     { name = "path" },
-                    { name = "buffer" },
+                    -- { name = "buffer" },
                     { name = "vim-dadbod-completion" },
                 }),
             })

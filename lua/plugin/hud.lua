ee62eab5ade718eef725601f555f50b34addaf58
Cameron Wong <cw@reosource.io>
2025-08-03 15:47:10 -0700

    Add window-picker support for neotree

--- lua/plugin/hud.lua
+++ lua/plugin/hud.lua
@@ -118,4 +118,13 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
     },
+    {
+        "s1n7ax/nvim-window-picker",
+        name = "window-picker",
+        event = "VeryLazy",
+        version = "2.*",
+        config = function()
+            require("window-picker").setup({ hint = "floating-big-letter" })
+        end,
+    },
 }

870dc0f6c4d0f6fa1f987cca07bd264a8bf06d98
Cameron Wong <cw@reosource.io>
2025-09-16 12:27:59 -0700

    Use vim-like keybinding to reopening file with Suda

--- lua/plugin/filetype.lua
+++ lua/plugin/filetype.lua
@@ -2,7 +2,7 @@ return {
     "lambdalisue/vim-suda",
     config = function()
         vim.g.suda_smart_edit = 1
-        vim.keymap.set("n", "<leader>ur", ":SudaRead<CR>", { desc = "Reopen file with sudo" })
+        vim.keymap.set("n", "<leader>ue", ":SudaRead<CR>", { desc = "Edit file with sudo" })
         vim.keymap.set("n", "<leader>uw", ":SudaWrite<CR>", { desc = "Write file with sudo" })
     end,
 }

373f486ec290179487cb781878e2f3972576d533
Cameron Wong <cw@reosource.io>
2025-10-30 06:15:14 -0700

    Add file ignore patterns for telescope

--- lua/plugin/find.lua
+++ lua/plugin/find.lua
@@ -1,87 +1,116 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', -- fuzzy finding
-        event = 'VimEnter',
-        branch = '0.1.x',
+        "nvim-telescope/telescope.nvim", -- fuzzy finding
+        event = "VimEnter",
+        branch = "0.1.x",
         dependencies = {
-            'nvim-lua/plenary.nvim',
+            "nvim-lua/plenary.nvim",
             {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'make',
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
                 cond = function()
-                    return vim.fn.executable 'make' == 1
+                    return vim.fn.executable("make") == 1
                 end,
             },
-            { 'nvim-telescope/telescope-ui-select.nvim' },
-            { 'nvim-tree/nvim-web-devicons', enabled = true },
+            { "nvim-telescope/telescope-ui-select.nvim" },
+            { "nvim-tree/nvim-web-devicons", enabled = true },
             {
-                'stevearc/aerial.nvim',
+                "stevearc/aerial.nvim",
                 opts = {},
                 dependencies = {
                     "nvim-treesitter/nvim-treesitter",
-                    "nvim-tree/nvim-web-devicons"
+                    "nvim-tree/nvim-web-devicons",
                 },
                 keys = {
-                    { '<leader>av', ':AerialNavToggle<CR>', desc = 'Aerial Nav Toggle' },
-                    { '<leader>aa', ':AerialToggle! left<CR>', desc = 'Aerial Toggle' },
-                    { '<leader>an', ':AerialNext<CR>', desc = 'Aerial Next' },
-                    { '<leader>ap', ':AerialPrev<CR>', desc = 'Aerial Prev' },
-                }
-
+                    { "<leader>av", ":AerialNavToggle<CR>", desc = "Aerial Nav Toggle" },
+                    { "<leader>aa", ":AerialToggle! left<CR>", desc = "Aerial Toggle" },
+                    { "<leader>an", ":AerialNext<CR>", desc = "Aerial Next" },
+                    { "<leader>ap", ":AerialPrev<CR>", desc = "Aerial Prev" },
+                },
             },
         },
         config = function()
-            require('telescope').setup {
+            require("telescope").setup({
                 extensions = {
-                    ['ui-select'] = {
-                        require('telescope.themes').get_dropdown(),
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown(),
                     },
                 },
-                aerial = {
+                aerial = {},
+                defaults = {
+                    file_ignore_patterns = {
+                        "%.img",
+                        "%.img.part%",
+                        "%.gif",
+                    },
                 },
-            }
+            })
 
             -- enable Telescope extensions if they are installed
-            pcall(require('telescope').load_extension, 'fzf')
-            pcall(require('telescope').load_extension, 'ui-select')
-            pcall(require('telescope').load_extension, 'aerial')
+            pcall(require("telescope").load_extension, "fzf")
+            pcall(require("telescope").load_extension, "ui-select")
+            pcall(require("telescope").load_extension, "aerial")
 
-            local builtin = require 'telescope.builtin'
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<C-f>', builtin.find_files)
-            vim.keymap.set('n', '<leader>ss', builtin.builtin,
-                { desc = '[S]earch [S]elect Telescope' })
-            vim.keymap.set('n', '<leader>sw', builtin.grep_string,
-                { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>sd', builtin.diagnostics,
-                { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-            vim.keymap.set('n', '<leader>s.', builtin.oldfiles,
-                { desc = '[S]earch Recent Files ("." for repeat)' })
-            vim.keymap.set('n', '<leader>sb', builtin.buffers,
-                { desc = '[ ] Find existing buffers' })
-            vim.keymap.set('n', '<leader>sz', function()
-                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+            vim.keymap.set("n", "<C-f>", builtin.find_files)
+            vim.keymap.set(
+                "n",
+                "<leader>ss",
+                builtin.builtin,
+                { desc = "[S]earch [S]elect Telescope" }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>sw",
+                builtin.grep_string,
+                { desc = "[S]earch current [W]ord" }
+            )
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set(
+                "n",
+                "<leader>sd",
+                builtin.diagnostics,
+                { desc = "[S]earch [D]iagnostics" }
+            )
+            vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+            vim.keymap.set(
+                "n",
+                "<leader>s.",
+                builtin.oldfiles,
+                { desc = '[S]earch Recent Files ("." for repeat)' }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>sb",
+                builtin.buffers,
+                { desc = "[ ] Find existing buffers" }
+            )
+            vim.keymap.set("n", "<leader>sz", function()
+                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                     winblend = 10,
                     previewer = false,
-                })
-            end, { desc = '[/] Fuzzily search in current buffer' })
+                }))
+            end, { desc = "[/] Fuzzily search in current buffer" })
 
-            vim.keymap.set('n', '<leader>s/', function()
-                builtin.live_grep {
+            vim.keymap.set("n", "<leader>s/", function()
+                builtin.live_grep({
                     grep_open_files = true,
-                    prompt_title = 'Live Grep in Open Files',
-                }
-            end, { desc = '[S]earch [/] in Open Files' })
+                    prompt_title = "Live Grep in Open Files",
+                })
+            end, { desc = "[S]earch [/] in Open Files" })
 
-            vim.keymap.set('n', '<leader>sn', function()
-                builtin.find_files { cwd = vim.fn.stdpath 'config' }
-            end, { desc = '[S]earch [N]eovim files' })
-            vim.keymap.set('n', '<leader>sa', ':Telescope aerial<CR>',
-                { desc = '[S]earch [A]erial' })
+            vim.keymap.set("n", "<leader>sn", function()
+                builtin.find_files({ cwd = vim.fn.stdpath("config") })
+            end, { desc = "[S]earch [N]eovim files" })
+            vim.keymap.set(
+                "n",
+                "<leader>sa",
+                ":Telescope aerial<CR>",
+                { desc = "[S]earch [A]erial" }
+            )
         end,
     },
 }
